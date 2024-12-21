05fff1f808e78f8ae6e8d6e53c4d04cd212df343
Sabir Ali <alisabir167167@gmail.com>
2024-12-21 06:46:28 +0530

    first Commit

---
+++ Forntend/app.js
@@ -0,0 +1,10 @@
+function personMaker(name,age){
+   const person={
+      name:name,
+      age:age,
+      talk(){
+     console.log(`Hi,my name is ${this.name}`);
+      },
+   };
+   return person;
+}
